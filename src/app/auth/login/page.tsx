c71c9b84b26035bdda14ef1741f4fc4a1d9c9bd6
nsshan98 <n.sakibnss@gmail.com>
2025-08-25 22:03:12 +0600

    feat: implement login page and login component

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginComponent from "@/components/auth/LoginComponent";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <LoginComponent />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/auth/LoginComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginComponent = () => {
+  return <div className="text-amber-100">LoginComponent</div>;
+};
+
+export default LoginComponent;
